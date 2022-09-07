17cd34203fb41ba8e56d405a40ff30b29a8d053a
Lazy-Bogdi <bmonote@normandiewebschool.fr>
2022-09-07 14:33:17 +0200

    ajout connexion

--- pages/espace_client.php
+++ pages/espace_client.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once 'fonction.php';
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -29,5 +35,28 @@
             </svg>
         </button>
     </nav>
+
+    <?php if(isset($_POST['user_pass']) && isset($_POST['user_nom']))
+{
+
+    $user_nom = $_POST['user_nom'];
+    $user_pass = $_POST['user_pass'];
+
+    $query=$bdd->prepare("SELECT id_client, user_client, pass_client
+			FROM clients");
+			$verif_exec = $query->execute();
+			$data = $query->fetch();
+
+    /*debug($data);*/
+    if ($data['user_client'] == $user_nom && $data['pass_client'] == $user_pass) {
+        echo "Bienvenue $user_nom";
+   
+    } else {
+        echo "<p> Mauvais identifiants. Retour au <a href='connexion.php'> menu de connexion </a><p>";
+    }
+}
+
+    ?>
+
 </body>
 </html>
---
+++ pages/fonction.php
@@ -0,0 +1,20 @@
+<?php
+/*---------------CONNEXION A LA BDD----------------*/
+try {
+		$bdd =new PDO("mysql:host=localhost;dbname=station_supervisor", "root", "");
+	}
+	catch(PDOException $e){
+		die('Erreur : SITE INDISPONIBLE ! ! ! ! ' . $e->getMessage());
+	}
+/*----------------FIN CONNEXION A LA BDD----------------*/
+
+
+/*FONCTION VARDUMP*/
+function debug($data) {
+	echo '<pre>';
+	var_dump($data);
+	echo'</pre>';
+	die(); 
+}
+
+?>
